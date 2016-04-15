d3c75b1066174b8b0156feea09c3f1ffdb672fa1
Andy <andrew.sunglae.kim@gmail.com>
2016-04-15 11:25:15 -0400

    add migrations and models

---
+++ db/migrate/20160415143111_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :album
+      t.string :preview_url
+      t.references :artist, index: true, foreign_key: true
+      t.timestamps null: false
+    end
+  end
+end
