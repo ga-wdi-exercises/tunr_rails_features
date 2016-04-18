502d7ffa5995f74119a64cbd919d712da94379bb
Andy <andrew.sunglae.kim@gmail.com>
2016-04-18 14:02:50 -0400

    add routes for new and delete

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,5 +6,24 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+  
+  def new
+    @song = Song.new
+  end
 
+  def create
+    @song = Song.create!(song_params)
+    redirect_to @song
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_path
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:name, :photo_url, :nationality)
+  end
 end
