60c598b1776ff4f9b72f355f8cd74a02d8546396
Andy <andrew.sunglae.kim@gmail.com>
2016-04-18 13:33:18 -0400

    adds create functionality

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,21 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
   end
 
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create!(artist_params)
+    redirect_to @artist
+  end
+
+  def destroy
+
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :photo_url, :nationality)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:index, :show]
-  resources :songs, only: [:index, :show]
+  resources :artists, only: [:index, :show, :new, :create, :destroy]
+  resources :songs, only: [:index, :show, :new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
